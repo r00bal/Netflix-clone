1c936792954153da6c3ecc2b36c924a88c3abb7a
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-11-09 23:35:10 +0100

    add SelectProfileContainer and Loading component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,30 @@
-import React, { useContext, useState } from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
+import { Loading, Header } from '../components';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+
+  useEffect(() => {
+    console.log(profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <p>Hello</p>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
