25b1a6e5fbbf9d64254688565ab57b3e5a1a34c6
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-11-19 19:27:45 +0100

    add Card test

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -82,7 +82,7 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
         <FeatureTitle>{itemFeature.title}</FeatureTitle>
         <FeatureText>{itemFeature.description}</FeatureText>
         <FeatureClose onClick={() => setShowFeature(false)}>
-          <img src="/images/icons/close.png" alt="close" />
+          <img src="/images/icons/close.png" alt="Close" />
         </FeatureClose>
         <Group margin="30px 0" flexDirection="row" alignItems="center">
           <Maturity rating={itemFeature.maturity}>{itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}</Maturity>
